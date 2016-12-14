5076a9ea1bcc6f23c46511793206d0ae5fadc5b5
Jacup <jacubgr@gmail.com>
2016-12-14 21:48:55 +0100

    b3 done

--- bash2/script9.sh
+++ bash2/script9.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-echo "podaj n"
+echo -e "Program liczy sume liczb parzystych < n.\nPodaj n."
 read n
+echo "Liczby, ktore bede sumowac:"
 m="$(( $n-2 ))"
 
   for (( i = 0; $i < $m;)); do
     i="$(( $i+2 ))"
     suma="$(( $suma+$i ))"
-    echo "$i = i , wynik = $suma"
+echo "$i"
 done;
-
-
+echo -e "\nSuma liczb parzystych mniejszych od '$n' wynosi: $suma"
+exit 0
   ## pokracznie, ale działa XD
   ## dodać ładne echa
